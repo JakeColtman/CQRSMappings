88ec1685905f2bb1ccbf64363011865e5bdb6bd1
Jake Coltman <=>
2015-11-05 08:11:07 +0000

    Added basic versioning to the repo (if this weren't play I'd break out the repo to DI in the saving functionality)

--- Mappings/Mappings/Repository.cs
+++ Mappings/Mappings/Repository.cs
@@ -11,11 +11,14 @@ namespace Mappings
 
         int[] _space;
         int _length;
+        IDictionary<int, int[]> _savedVersions;
+        int _version;
 
         public Repository(int length)
         {
             _length = length;
             _space = new int[length];
+            _version = 1;
         }
 
         public bool add_item(int pos, int value, bool overWrite)
@@ -33,6 +36,20 @@ namespace Mappings
                 return false;
             }
             
+        }
+
+        public int save_and_return_id()
+        {
+            _savedVersions[_version] = _space;
+            _version += 1;
+            return _version;
+        }
+
+        public bool load_from_version_id(int version)
+        {
+            save_and_return_id();
+            _space = _savedVersions[version];
+            return true;
         }
 
         public int[] get_data()
