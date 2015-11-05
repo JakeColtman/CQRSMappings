fffef8299ae27d0bc290b8dc019a8312e9c45fdb
Jake Coltman <=>
2015-11-05 08:06:29 +0000

    Added threatened projection

--- Mappings/Mappings/Program.cs
+++ Mappings/Mappings/Program.cs
@@ -17,10 +17,16 @@ namespace Mappings
 
             IProjection valueProjection = new Projections.AllUnitProjection(repo);
             IProjection occupiedProjection = new Projections.SpaceOccupiedProjection(repo);
+            IProjection threatenedProjection = new Projections.ThreatenedProjection(repo);
             Console.WriteLine("Values");
             valueProjection.present();
+            Console.WriteLine("");
             Console.WriteLine("Occupation");
             occupiedProjection.present();
+            Console.WriteLine("");
+            Console.WriteLine("Threatened");
+            threatenedProjection.present();
+            Console.WriteLine("");
             Console.Read();
 
         }
---
+++ Mappings/Mappings/Projections/ThreatenedProjection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mappings.Projections
+{
+    class ThreatenedProjection : IProjection
+    {
+
+        IRepositoryReader _repo;
+
+        public void present()
+        {
+            int[] repoData = _repo.get_data();
+            int[] output = new int[repoData.Length];
+            for( int ii = 0; ii < repoData.Length; ii++)
+            {
+                if (repoData[Math.Max(0, ii - 1)] > 0 || repoData[Math.Min(ii + 1, repoData.Length - 1)] > 0) Console.Write(1);
+                else Console.Write(0);
+            }
+            
+        }
+
+        public ThreatenedProjection(IRepositoryReader repo)
+        {
+            _repo = repo;
+        }
+    }
+}
