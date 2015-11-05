8506e8a63a7e72f51e7bf1b6d6da39b158087d6f
Jake Coltman <=>
2015-11-05 07:21:23 +0000

    Added basic creation event

---
+++ Mappings/Mappings/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mappings
+{
+    public class Message
+    {
+    }
+
+    public class ObjectCreationMessage
+    {
+        int pos { get; set; }
+        int value { get; set; }
+    }
+
+}
