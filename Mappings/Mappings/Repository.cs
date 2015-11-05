e3bb509391f506e42d60d76d331ecc9790b46198
Jake Coltman <=>
2015-11-05 07:27:51 +0000

    Added repo to hold the data

---
+++ Mappings/Mappings/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mappings
+{
+    class Repository
+    {
+
+        int[] _space;
+        int _length;
+
+        public Repository(int length)
+        {
+            _length = length;
+            _space = new int[length];
+        }
+
+        public bool add_item(int pos, int value, bool overWrite)
+        {
+            if (pos < 0) throw new ArgumentOutOfRangeException("pos", "position needs to be positive");
+            if (pos < _length) throw new ArgumentOutOfRangeException("pos", "position needs to be less than " + _length.ToString());
+            if (overWrite || _space[pos] == 0)
+            {
+                _space[pos] = value;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Space occupied");
+                return false;
+            }
+            
+        }
+
+    }
+}
